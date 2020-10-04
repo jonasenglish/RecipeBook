5df9913b2873b0f71785c6621f62330bf739e8a8
Jonas English <JonasEnglish0@gmail.com>
2020-10-04 11:14:13 -0500

    First Commit

---
+++ RecipeBook/src/application/RecipeBookController.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+public class RecipeBookController implements Initializable {
+	
+	@FXML private TableView<String> tableView;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
